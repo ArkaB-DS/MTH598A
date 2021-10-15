ffc39f9e5ff03ec3cb26b6a616c04e821805d43c
Arkajyoti Bhattacharjee <arkastat98@gmail.com>
2021-10-15 23:31:45 +0530

    modified code for adaptive random walk

--- LogisticRegressionExample.R
+++ LogisticRegressionExample.R
@@ -49,42 +49,48 @@ data_X = data.matrix(data_X)
 X = data_X
 Y = data.matrix(ifelse(data[,14]==0,0,1))
 
-N = 1e6
-d=13
-s=20
+N = 1e5
+d = 13
+s = 20
 
-beta = matrix(0,nrow=N,ncol=d)
+beta = matrix(0, nrow=N,ncol=d)
+beta[1,] = rnorm( d , 0 , 0.1 / sqrt(d) )
+Sigma = 2.38^2/d*diag(d)
+
 acc.prob = numeric(N)
 
-c = -1
-Sigma=exp(c)*diag(d)
-mu = rep(0,d)
-
-f <- function(beta) (sum(dbinom(Y,size=1,prob=1/(1+exp(-X%*%beta)),log=TRUE)))+sum(dnorm(beta,0,s,log=TRUE))
+# c = -2.3
+# Sigma = exp(c)*diag(d)
+f <- function(beta) sum(dbinom(Y,size=1,prob=1/(1+exp(-X%*%beta)),log=TRUE))+sum(dnorm(beta,0,s,log=TRUE))
 
 for(i in 2:N){
-  if(i%%1e3==0) print(i/1e3)
-  y = beta[i-1,] + as.vector(mvtnorm::rmvnorm(1,mean=mu,sigma=Sigma))
-  alpha = f(y) - f(beta[i-1,])
+  if( i %% 1e3 == 0) {
+    print( i / 1e3)
+    cat("AP= ",mean(acc.prob[1:i]),"\n")
+  }
+  if( i < 2*d ) {
+    y = rnorm( d , beta[i-1,] , 0.1 / sqrt(d) )
+  }else {
+    b = rbinom(1,size=1,prob = 0.05)
+    Sigma = cov(beta[1:(i-1),])
+    y = b * rnorm(d,beta[i-1,],0.1/sqrt(d)) + (1 - b) * as.vector(mvtnorm::rmvnorm(1,mean = beta[i-1,],sigma = 2.38^2*Sigma/d))
+  }
+  alpha = f(y) / f(beta[i-1,])
+
   if(runif(1) < alpha){
     beta[i,] = y
     acc.prob[i] = 1
   }else{
     beta[i,] = beta[i-1,]
   }
-  if(isSymmetric(Sigma + 1/i*(tcrossprod(beta[i,]-mu)-Sigma)) &
-   min(eigen(Sigma + 1/i*(tcrossprod(beta[i,]-mu)-Sigma))$values)>0.5){
-    Sigma = Sigma + 1/i*(tcrossprod(beta[i,]-mu)-Sigma)
-   }
-  mu = mu + 1/i*(beta[i,]-mu)
-  if ((c + 1/i*(alpha-0.23))>=-20 & (c + 1/i*(alpha-0.23))<=20){
-   c = c + 1/i*(alpha-0.23)
-  }
-  # Sigma = exp(c)*Sigma
-  Sigma = c*Sigma
+ # Sigma = cov(beta[1:i,]) + diag(1e-3,d)
+  # if ( (c + 1 / i * (alpha-0.23)) >= -5 & (c + 1 / i * (alpha-0.23)) <= 5 ){
+  #   c = c + 1 / i * (alpha-0.23)
+  # }
+  # Sigma=exp(c)*Sigma
+
 }
 mean(acc.prob)
-
 plot(cumsum(acc.prob)/(1:N),col="red",type="l",xlab="",ylab="")
 abline(h=0.23,col="blue",lty=2)
 
